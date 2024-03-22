5be15bdef50793cd7ea3b41bdf1ec15857331d30
hpsprger <281872614@qq.com>
2024-03-22 12:41:14 +0800

    页表映射添加注释

--- xen/arch/arm/include/asm/early_printk.h
+++ xen/arch/arm/include/asm/early_printk.h
@@ -25,8 +25,8 @@
 
     EARLY_UART_VIRTUAL_ADDRESS ==> (FIXMAP_ADDR(FIXMAP_CONSOLE) + (CONFIG_EARLY_UART_BASE_ADDRESS & ~PAGE_MASK)) 
                                    ((FIXMAP_VIRT_START + (FIXMAP_CONSOLE) * PAGE_SIZE) + (CONFIG_EARLY_UART_BASE_ADDRESS & ~PAGE_MASK)) 
-                                   ((0x20000400000 + 0 * 4K) + (0xff1a0000 & 0xfffffffffffff000)) 
-                                   0x20000400000 + 0xff1a0000 ==> 0x200FF5A0000
+                                   ((0x20000400000 + 0 * 4K) + (0xff1a0000 & ~0xfffffffffffff000)) 
+                                   0x20000400000 + 0x0 ==> 0x20000400000 
 */
 #define EARLY_UART_VIRTUAL_ADDRESS \
     (FIXMAP_ADDR(FIXMAP_CONSOLE) + (CONFIG_EARLY_UART_BASE_ADDRESS & ~PAGE_MASK))
